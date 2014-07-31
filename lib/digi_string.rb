3989093afb662799d2faec12b50bbb1ede88cf0f
kyoendo <postagie@gmail.com>
2014-07-31 22:12:29 +0900

    add DigiString::String.new

--- lib/digi_string.rb
+++ lib/digi_string.rb
@@ -4,5 +4,12 @@ require "digi_string/char"
 require "colcolor"
 
 module DigiString
-  # Your code goes here...
+  class String
+    attr_reader :map
+    def initialize(str, space:1, **opts)
+      head, *rest = str.to_s.each_char.map { |chr| Char[chr.intern, opts] }
+      joint = (" " * space).colco(opts[:bg], regexp:/./)
+      @map = head.zip(*rest).map { |e| e.join(joint) }
+    end
+  end
 end
--- spec/digi_string_spec.rb
+++ spec/digi_string_spec.rb
@@ -5,3 +5,16 @@ describe DigiString do
     expect(DigiString::VERSION).not_to be nil
   end
 end
+
+describe DigiString::String do
+  before do
+    @str_map = ["  \e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m             ", "\e[47m \e[0m\e[47m \e[0m      \e[47m \e[0m\e[47m \e[0m           ", "\e[47m \e[0m\e[47m \e[0m      \e[47m \e[0m\e[47m \e[0m \e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m    ", "\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m       \e[47m \e[0m\e[47m \e[0m  ", "\e[47m \e[0m\e[47m \e[0m      \e[47m \e[0m\e[47m \e[0m   \e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m  ", "\e[47m \e[0m\e[47m \e[0m      \e[47m \e[0m\e[47m \e[0m \e[47m \e[0m\e[47m \e[0m    \e[47m \e[0m\e[47m \e[0m  ", "\e[47m \e[0m\e[47m \e[0m      \e[47m \e[0m\e[47m \e[0m   \e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m\e[47m \e[0m  "]
+  end
+
+  describe ".new" do
+    it "builds string map" do
+      str = DigiString::String.new("Aa")
+      expect(str.map).to eq @str_map
+    end
+  end
+end
